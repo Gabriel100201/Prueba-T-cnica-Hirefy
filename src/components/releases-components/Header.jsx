dc93de768f60faa1470803b9c37d3c3c9fad2d20
Gabriel <gabrielfunes100202@gmail.com>
2024-07-06 13:23:56 -0300

    Arreglo sorter de Releases

--- src/components/releases-components/Header.jsx
+++ src/components/releases-components/Header.jsx
@@ -1,4 +1,10 @@
-export const Header = () => {
+// eslint-disable-next-line react/prop-types
+export const Header = ({ filterStatus, handleFilterChange }) => {
+  const handleChange = (event) => {
+    const selectedStatus = event.target.value;
+    handleFilterChange(selectedStatus === 'all' ? null : selectedStatus);
+  };
+
   return (
     <div className="px-[35px] mt-[20px] xl:mt-[80px] text-alternative flex flex-col xl:flex-row xl:items-center xl:justify-between">
       <h4 className="text-[20px] xl:text-[44px] xl:font-[400] font-bold">
@@ -6,10 +12,10 @@ export const Header = () => {
       </h4>
       <div className="custom-select-container mt-[10px]">
         <label htmlFor="filter" className="text-[14px] font-semibold mr-[10px]">Sort By:</label>
-        <select name="filter" id="filter" className="custom-select w-[182px] h-[39px] rounded-[30px] border-[1px] border-[#DEE7F0] px-[18px]">
+        <select value={filterStatus || "All"} name="filter" id="filter" className="custom-select w-[182px] h-[39px] rounded-[30px] border-[1px] border-[#DEE7F0] px-[18px]" onChange={handleChange}>
           <option value="all">All</option>
-          <option value="1">Option-1</option>
-          <option value="2">Option-2</option>
+          <option value="Feat">Feat</option>
+          <option value="Fix">Fix</option>
         </select>
       </div>
     </div>
--- src/components/sections/Releases.jsx
+++ src/components/sections/Releases.jsx
@@ -1,13 +1,21 @@
+import { useState } from "react"
 import { DividerRelease } from "../releases-components/Divider"
 import { Header } from "../releases-components/Header"
 import { StepperRelease } from "../releases-components/StepperRelease"
 
 export const Releases = () => {
+
+  const [filterStatus, setFilterStatus] = useState(null);
+
+  const handleFilterChange = (status) => {
+    setFilterStatus(status);
+  };
+
   return (
     <section>  
-      <Header></Header>
+      <Header filterStatus={filterStatus} handleFilterChange={handleFilterChange}></Header>
       <DividerRelease></DividerRelease>
-      <StepperRelease></StepperRelease>
+      <StepperRelease filterStatus={filterStatus}></StepperRelease>
     </section>
   )
 }
